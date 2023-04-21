9227584139b5494d3ed880061766557385ba5020
wughangar <wughangar.rose@gmail.com>
2023-04-21 11:24:21 +0000

    updated changes

--- cargs.c
+++ cargs.c
@@ -9,94 +9,19 @@
  * @path: pointer to the path
  * Return: pointer to the full path
  */
-char *find_path(char *arg, char *path)
-{
-        char *fpath = NULL;
-        char *dir = strtok(path, ":");
-        while (dir != NULL)
-        {
-                fpath = malloc(strlen(dir) + strlen(arg) + 2);
-                sprintf(fpath, "%s/%s", dir, arg);
-                if (access(fpath, X_OK) == 0)
-                        return (fpath);
-                free(fpath);
-                dir = strtok(NULL, ":");
-        }
-        return (NULL);
-}
-/**
- * main - reads user input and executes user commands using execve
- * Return: nothing
- */
 
 char *find_path(char *arg, char *path);
 
 int main(int argc, char **argv, char **envp)
 {
-<<<<<<< HEAD
 	char *args[1024];
-	int i = 0;
 	char *temp = NULL;
 	char *commands = NULL;
-	char *tok = NULL;
-=======
-<<<<<<< HEAD
-        char *temp = "";
-        size_t len = 0;
-        pid_t pid;
-        ssize_t read;
-        char *tok = NULL;
-        char *args[1024];
-        int i = 0;
         char *cpath = NULL;
-
-        while(1)
-        {
-                printf("$ ");
-                read = getline(&temp, &len, stdin);
-
-                if (read == -1)
-                {
-                        printf("\n");
-                        break;
-                }
-
-                temp[strcspn(temp, "\n")] = '\0';
-                tok = strtok(temp, " ");
-                while (tok != NULL)
-                {
-                        args[i++] = tok;
-                        tok = strtok(NULL, " ");
-                }
-                args[i] = NULL;
-
-                cpath = find_path(args[0], cpath);
-                if (cpath != NULL)
-                {
-                        pid = fork();
-                        if (pid == 0)
-                        {
-                                execve(cpath, args, NULL);
-                                perror("execve process failed");
-                                exit(EXIT_FAILURE);
-                        }
-                        else if (pid < 0)
-                                perror("fork process failed");
-                        else
-                                waitpid(pid, NULL, 0);
-                        free(cpath);
-                }
-                else
-                        printf("%s: no such command or directory found\n", args[0]);
-        }
-        free(temp);
-        return (0);
-=======
-	char *temp, *args[1024], *cpath;
->>>>>>> 707f8a52ccd19fcfa04c9294736acdb686185175
+	char *tok = NULL;
 	size_t len = 0;
 	pid_t pid;
-	char *cpath = NULL;
+	int i = 0;
 	int j = 0;
 	FILE *fp;
 
@@ -104,15 +29,13 @@ int main(int argc, char **argv, char **envp)
 	{
 		for (i = 1; i < argc && i < 1023; i++)
 		{
-			args[i - 1] = argv[i];
+			args[i -1] = argv[1];
 		}
-		args[i - 1] = argv[1];
+		args[1 - 1] = NULL;
 	}
-	args[i -1] = NULL;
-
-	temp = (char *) malloc(len + 1);
-	if (temp == NULL)
-		exit(1);
+		temp = (char *)malloc(len + 1);
+		if (temp == NULL)
+			exit(1);
 
 	while (1)
 	{
@@ -219,5 +142,4 @@ char *find_path(char *arg, char *path)
 	}
 	free(pcopy);
 	return (NULL);
->>>>>>> 8871f5191cc49b1c43182675871dbe4b1807d228
 }
