6dfda5c629d87d3e5d257f0c0edccd7d23e0f4c1
wughangar <wughangar.rose@gmail.com>
2023-04-21 10:43:47 +0000

    fixed errors concerning command line arguments

--- cargs.c
+++ cargs.c
@@ -11,20 +11,35 @@
 
 char *find_path(char *arg, char *path);
 
-int main(__attribute__((unused))int ac, __attribute__((unused))char **av, char **envp)
+int main(int argc, char **argv, char **envp)
 {
-	char *temp, *args[1024], *cpath;
+	char *args[1024];
+	int i = 0;
+	char *temp = NULL;
+	char *commands = NULL;
+	char *tok = NULL;
 	size_t len = 0;
 	pid_t pid;
-	char *tok;
-	int i = 0;
-	char **j;
+	char *cpath = NULL;
+	int j = 0;
+	FILE *fp;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc && i < 1023; i++)
+		{
+			args[i - 1] = argv[i];
+		}
+		args[i - 1] = argv[1];
+	}
+	args[i -1] = NULL;
+
 	temp = (char *) malloc(len + 1);
 	if (temp == NULL)
 		exit(1);
 
 	while (1)
-	{	
+	{
 	printf("$ ");
 	
 	if (getline(&temp, &len, stdin) == -1)
@@ -32,20 +47,26 @@ int main(__attribute__((unused))int ac, __attribute__((unused))char **av, char *
 	
 
 	temp[strcspn(temp, "\n")] = '\0';
+	
 
 	if(strcmp(temp, "exit") == 0)
-	{
+	
 		break;
-	}
 
 	if (strcmp(temp, "env") == 0)
 	{
-		for (j = envp; *j != NULL; j++)
-			printf("%s\n", *j);
-		continue;
+	
+		for (j = 0; envp[j] != NULL; j++)
+			printf("%s\n", envp[j]);
+	continue;
 	}
 
-	tok = strtok(temp, " ");
+	commands =  strtok(temp, ";");
+
+	while (commands != NULL)
+	{
+	tok = strtok(commands, " ");
+	i = 0;
 	while (tok != NULL && i < 1023)
 	{
 		args[i++] = tok;
@@ -53,21 +74,35 @@ int main(__attribute__((unused))int ac, __attribute__((unused))char **av, char *
 	}
 	args[i] = NULL;
 
+	if (i > 0 && strcmp(args[i -1], "-") == 0 && i < 1023)
+	{
+		args[i -1] = NULL;
+		fp = freopen(args[i - 2], "r", stdin);
+		if (fp == NULL)
+		{
+			printf("Error opening file\n");
+			continue;
+		}
+	}
+
 	cpath = getenv("PATH");
 	cpath = find_path(args[0], cpath);
 
 	if (cpath == NULL)
 	{
 		printf("%s: command not found\n", args[0]);
+		commands = strtok(NULL, ";");
 		continue;
 	}
 
 	pid = fork();
 	if (pid == 0)
-		execve(cpath, args, (char* const*) envp);
+		execve(cpath, args, envp);
 	else
 		wait(NULL);
+	commands = strtok(NULL, ";");
 	}
+}
 	free(temp);
 	return(0);
 }
@@ -76,19 +111,36 @@ int main(__attribute__((unused))int ac, __attribute__((unused))char **av, char *
 char *find_path(char *arg, char *path)
 {
 	char *fpath = NULL;
-	char *dir = strtok(path, ":");
+	char *dir =NULL;
+	char *pcopy = NULL;
+
+	pcopy = strdup(path);
+	if(pcopy == NULL)
+	{
+		exit(1);
+	}
+
+	dir = strtok(pcopy, ":");
 
 	while (dir != NULL)
 	{
-		fpath = (char *) malloc(strlen(dir) +strlen(arg) + 2);
+		fpath = malloc(strlen(dir) +strlen(arg) + 2);
 		if (fpath == NULL)
+		{
 			exit(1);
+		}
+
 		sprintf(fpath, "%s/%s", dir, arg);
+
 		if (access(fpath, F_OK) == 0)
+		{
+			free(pcopy);
 			return (fpath);
+		}
+
 		free(fpath);
-		fpath = NULL;
 		dir = strtok(NULL, ":");
 	}
+	free(pcopy);
 	return (NULL);
 }
